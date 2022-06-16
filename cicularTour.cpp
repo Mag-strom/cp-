876e42db34ebe790623038993cf6832ba908cb66
Anish Goud <107237151+Mag-strom@users.noreply.github.com>
2022-06-16 20:20:22 +0530

    Circular Tour(sliding window)

---
+++ cicularTour.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+ // } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int start =0,extra=0,defict=0;
+       for(int i=0;i<n;i++)
+       {
+           extra+=p[i].petrol-p[i].distance;
+           if(extra<0)
+           {
+               start=i+1;
+               defict+=(-1*extra);
+               extra=0;
+           }
+       }
+       if(extra>=defict)
+       return start;
+       else
+       return -1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+  // } Driver Code Ends
